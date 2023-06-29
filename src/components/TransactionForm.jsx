b55f5092b942b0c7f2e1725114c529a6f2e907aa
nkwaaaa <nkwaaaa@gmail.com>
2023-06-29 12:17:07 -0300

    add new transaction logic

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,3 +1,38 @@
+import { useState } from "react"
+import { useGlobalState } from "../context/GlobalContext"
+
 export default function TransactionForm() {
-  return <div>TransactionForm</div>
+  const [description, setDescription] = useState("")
+  const [amount, setAmount] = useState(0)
+  const { addTransaction, transactions } = useGlobalState()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    addTransaction({
+      id: 1,
+      description,
+      amount,
+    })
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          placeholder="Enter description"
+        />
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          type="number"
+          step={0.01}
+          placeholder="00.00"
+        />
+        <button>Add Transaction</button>
+      </form>
+
+      <h2>{JSON.stringify(transactions)}</h2>
+    </div>
+  )
 }
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -12,8 +12,20 @@ const initialState = {
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
 
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    })
+  }
+
   return (
-    <Context.Provider value={{ transactions: state.transactions }}>
+    <Context.Provider
+      value={{
+        transactions: state.transactions,
+        addTransaction,
+      }}
+    >
       {children}
     </Context.Provider>
   )
